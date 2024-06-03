64f59e80b05acbb4ac9f1e3ddd50a0d74c2abdac
mikeworldyt <diazmig98@gmail.com>
2024-06-03 09:29:49 -0600

    py script for level 1

---
+++ create-new-ANT.py
@@ -0,0 +1,18 @@
+import os
+
+# Make Dir of Level 1
+os.makedirs('Root_Storage', exist_ok=True)
+os.makedirs('Root_Storage/001.PERSONAL', exist_ok=True)
+os.makedirs('Root_Storage/002.WORK', exist_ok=True)
+os.makedirs('Root_Storage/003.PROJECTS', exist_ok=True)
+os.makedirs('Root_Storage/004.ACADEMIC', exist_ok=True)
+os.makedirs('Root_Storage/005.HOBBIES', exist_ok=True)
+os.makedirs('Root_Storage/900.Resources', exist_ok=True)
+os.makedirs('Root_Storage/998.Review', exist_ok=True)
+os.makedirs('Root_Storage/999.Setting', exist_ok=True)
+
+print(' ')
+print('------- Successfully executed script [100%] -------')
+print(' ')
+print('   Please Close this window.')
+print('   If you need help, Read the documentation: https://github.com/MikeWorldYt/ANT-on-the-system')
