607b180cefb4d1001b4c9275b75e335124ea8364
mikeworldyt <diazmig98@gmail.com>
2024-06-18 22:47:36 -0600

    routing home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,13 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.component'),
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
 ];
