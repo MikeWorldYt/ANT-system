71d79f7d56dbf492ac4af896dac57b7a934fdca4
mikeworldyt <diazmig98@gmail.com>
2024-06-20 08:40:00 -0600

    Roter works (bug: component default still there)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,11 @@ import { Docs_T01_IntoComponent } from './docs/t01-starter/into-getting-starter/
 import { Docs_T02_IntoComponent } from './docs/t02-concepts/into-main-concepts/into-main-concepts.component';
 
 export const routes: Routes = [
-  {  path: 'home', component: HomeComponent  },
-  {  path: 'docs/introduction', component: Docs_T01_IntoComponent  },
-  {  path: 'docs/concepts', component: Docs_T02_IntoComponent  },
-  {
-    path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
-  }
+  {  path: 'home', component: HomeComponent,  
+    children: [
+      {  path: 'docs/introduction', component: Docs_T01_IntoComponent  },
+      {  path: 'docs/concepts', component: Docs_T02_IntoComponent  },
+    ]
+  },
+  { path: '', redirectTo: '/home',  pathMatch: 'full'  }
 ];
--- src/app/layout/nav/nav.component.ts
+++ src/app/layout/nav/nav.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
   imports: [
     RouterOutlet,
-    RouterLink
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
