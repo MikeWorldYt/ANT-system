a975cb7b41f8ef9887d81062f4f17135d655a58c
mikeworldyt <diazmig98@gmail.com>
2024-06-20 13:05:55 -0600

    fill in

--- src/app/docs/t02-concepts/into-main-concepts/into-main-concepts.component.ts
+++ src/app/docs/t02-concepts/into-main-concepts/into-main-concepts.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../../../layout/footer/footer.component';
 
 @Component({
   selector: 'docs-t02-into-main-concepts',
   standalone: true,
-  imports: [],
+  imports: [
+    FooterComponent
+  ],
   templateUrl: './into-main-concepts.component.html',
   styleUrl: '../../docs.component.css'
 })
