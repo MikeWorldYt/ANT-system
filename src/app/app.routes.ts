af7fa351457cadb4947de73f243eda5504f71871
mikeworldyt <diazmig98@gmail.com>
2024-07-06 13:33:46 -0600

    refactorized, cleaned

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
-import HomeComponent from './pages/home/home.component';
-import { Docs_T01_IntoComponent } from './docs/t01-starter/into-getting-starter/into-getting-starter.component';
-import { Docs_T02_IntoComponent } from './docs/t02-concepts/into-main-concepts/into-main-concepts.component';
 import { NgModule } from '@angular/core';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
-import { NotFoundComponentComponent } from './pages/error/404/not-found.component';
+
+import { Docs_T01_IntoComponent } from './docs/t01-starter/into-getting-starter/into-getting-starter.component';
+import { Docs_T02_IntoComponent } from './docs/t02-concepts/into-main-concepts/into-main-concepts.component';
+
 import { AppComponent } from './app.component';
+import HomeComponent from './pages/home/home.component';
+import { NotFoundComponentComponent } from './pages/error/404/not-found.component';
 
 export const routes: Routes = [
   {  path: '', component: HomeComponent,  
@@ -44,4 +46,4 @@ export const routes: Routes = [
   bootstrap: [AppComponent]
 })
 
-export class AppRoutingModule { }5
+export class AppRoutingModule { }
