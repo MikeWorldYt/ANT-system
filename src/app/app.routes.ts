e9221fbe616f758d5ac58615fbd611cdbaa1ae82
mikeworldyt <diazmig98@gmail.com>
2024-07-08 19:58:04 -0600

    routing issue solved

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,25 @@
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+
 // import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { Docs_T01_IntoComponent } from './docs/t01-starter/into-getting-starter/into-getting-starter.component';
 import { Docs_T02_IntoComponent } from './docs/t02-concepts/into-main-concepts/into-main-concepts.component';
 
-import { AppComponent } from './app.component';
 import HomeComponent from './pages/home/home.component';
 import { NotFoundComponentComponent } from './pages/error/404/not-found.component';
 
 export const routes: Routes = [
-  {  path: '', component: HomeComponent,  
+  { path: '', component: HomeComponent,  
     children: [
-      { path: '', redirectTo: 'docs/introduction', pathMatch: 'full' },
-      { path: 'docs', redirectTo: 'docs/introduction', pathMatch: 'full' },
-      { path: 'docs/introduction', component: Docs_T01_IntoComponent },
-      { path: 'docs/concepts', component: Docs_T02_IntoComponent },
+      { path: 'introduction', component: Docs_T01_IntoComponent },
+      { path: 'concepts', component: Docs_T02_IntoComponent },
+      { path: '', redirectTo: 'introduction', pathMatch: 'full' },
     ]
   },
   { path: 'not-found', component: NotFoundComponentComponent },
-  { path: '**', component: NotFoundComponentComponent,  pathMatch: 'full'  }
+  { path: '**', redirectTo: 'not-found', pathMatch: 'full' },
 ];
 
 @NgModule({
@@ -28,7 +27,7 @@ export const routes: Routes = [
   imports:[
     BrowserModule,
     RouterModule.forRoot(routes, {
-      useHash: true
+      useHash: true,
     })
   ],
   providers: [
@@ -36,8 +35,7 @@ export const routes: Routes = [
     //   provide: LocationStrategy,
     //   useClass: HashLocationStrategy
     // }
-  ],
-  bootstrap: [AppComponent]
+  ]
 })
 
 export class AppRoutingModule { }
