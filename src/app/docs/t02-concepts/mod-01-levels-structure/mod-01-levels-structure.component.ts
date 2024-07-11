dc55b5150774a78eb6dd9455fcaa9e2193b617d6
mikeworldyt <diazmig98@gmail.com>
2024-07-11 15:21:10 -0600

    breadcrumb input

--- src/app/docs/t02-concepts/mod-01-levels-structure/mod-01-levels-structure.component.ts
+++ src/app/docs/t02-concepts/mod-01-levels-structure/mod-01-levels-structure.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderT02Component } from '../header-t02/header-t02.component';
 
 @Component({
   selector: 'doc-t02-mod-01-levels-structure',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderT02Component
+  ],
   templateUrl: './mod-01-levels-structure.component.html',
   styleUrl: '../../docs.component.css'
 })
