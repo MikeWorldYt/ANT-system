d5e7af794207d1deec6f95c878b81c3519f59c88
mikeworldyt <diazmig98@gmail.com>
2024-07-11 15:02:22 -0600

    contents reorganized

--- src/app/docs/t02-concepts/into-main-concepts/into-main-concepts.component.ts
+++ src/app/docs/t02-concepts/into-main-concepts/into-main-concepts.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../../layout/footer/footer.component';
+import { Docs_T02_Mod01_Component } from '../mod-01-levels-structure/mod-01-levels-structure.component';
 
 @Component({
   selector: 'docs-t02-into-main-concepts',
   standalone: true,
   imports: [
-    FooterComponent
+    FooterComponent,
+    Docs_T02_Mod01_Component,
   ],
   templateUrl: './into-main-concepts.component.html',
   styleUrl: '../../docs.component.css'
--- src/app/docs/t02-concepts/mod-01-levels-structure/mod-01-levels-structure.component.ts
+++ src/app/docs/t02-concepts/mod-01-levels-structure/mod-01-levels-structure.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-mod-01-levels-structure',
+  selector: 'doc-t02-mod-01-levels-structure',
   standalone: true,
   imports: [],
   templateUrl: './mod-01-levels-structure.component.html',
   styleUrl: '../../docs.component.css'
 })
-export class Mod01LevelsStructureComponent {
+export class Docs_T02_Mod01_Component {
 
 }
