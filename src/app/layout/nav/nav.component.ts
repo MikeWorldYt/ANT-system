8590014adb5d006ae61af9da2bd692285ec54faa
mikeworldyt <diazmig98@gmail.com>
2024-07-12 12:55:32 -0600

    section controller progress

--- src/app/layout/nav/nav.component.ts
+++ src/app/layout/nav/nav.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
@@ -25,6 +25,29 @@ export class NavComponent {
     } else {
       this.currentSection = section; // Si se hace clic en una nueva sección, la mostramos
     }
+  }
+
+  // Module section control
+  moduleSection: string = '';
+
+  @HostListener('window:scroll', [])
+onWindowScroll() {
+  const sections = document.querySelectorAll('h2');
+  sections.forEach(section => {
+    const rect = section.getBoundingClientRect();
+    if (rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)) {
+      this.moduleSection = section.id;
+    }
+  });
+}
+
+  scrollToSection(sectionId: string) {
+    document.getElementById(sectionId)?.scrollIntoView({ behavior: 'smooth' });
+    this.moduleSection = sectionId;
+  }
+
+  moduletoggle(section: string) {
+    this.moduleSection = section;
   }
 
 }
