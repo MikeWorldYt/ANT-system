afc78b183f54eb2ba1d546ad65c4cd7c0c814eb5
mikeworldyt <diazmig98@gmail.com>
2024-07-11 20:51:02 -0600

    markdown hash style integration

--- src/app/docs/t02-concepts/mod-01-levels-structure/mod-01-levels-structure.component.ts
+++ src/app/docs/t02-concepts/mod-01-levels-structure/mod-01-levels-structure.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
 import { HeaderT02Component } from '../header-t02/header-t02.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'doc-t02-mod-01-levels-structure',
   standalone: true,
   imports: [
-    HeaderT02Component
+    HeaderT02Component,
+    CommonModule
   ],
   templateUrl: './mod-01-levels-structure.component.html',
   styleUrl: '../../docs.component.css'
 })
 export class Docs_T02_Mod01_Component {
+  hovered = false;
 
+showHash(event: Event) {
+  this.hovered = true;
+}
+
+hideHash(event: Event) {
+  this.hovered = false;
+}
 }
