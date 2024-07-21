4eb2687ad8faf2200e9dc2b89b66e64dac1e9e71
mikeworldyt <diazmig98@gmail.com>
2024-07-20 22:58:11 -0600

    fill-in

--- src/app/docs/content/content.ts
+++ src/app/docs/content/content.ts
@@ -11,13 +11,27 @@ export const content = {
       module_01: {
 				section10: {
 					h1: '1. Levels and Directory Structure',
-					p1: "Levels refer to the depth within the folder tree directory.<strong>Chill Structure</strong>",
+					p1: 'Levels refer to the depth within the folder tree directory.',
+					p2: '<strong>Control Structure</strong>',
 				}
       }
     }
   },
   ES: {
-    value1: "Contenido",
-    value2: "Contenido JSON funciona."
+    title_01: {
+			module_01: {
+				section_01 : {
+					h1: '¿Que es ANT?',
+				}
+			},
+		},
+		title_02: {
+			module_01: {
+				section10: {
+					h1: '1. Estructura de Niveles y Estructura de Carpetas',
+					p1: 'Niveles refieren a la profundidad dentro de la estructura de carpetas directorio.',
+				}
+			}
+		}
   }
 };
