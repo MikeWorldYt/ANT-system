5ef96008efe64405c0bdfb7a5d89e5e899aaa4c7
mikeworldyt <diazmig98@gmail.com>
2024-07-20 21:44:30 -0600

    fill-in (first commit)

---
+++ src/app/docs/content/content.ts
@@ -0,0 +1,23 @@
+export const content = {
+  EN: {
+    title_01: {
+      module_01: {
+				section_01 : {
+					h1: 'What is ANT?',
+				}
+      },
+    },
+    title_02: {
+      module_01: {
+				section10: {
+					h1: '1. Levels and Directory Structure',
+					p1: "Levels refer to the depth within the folder tree directory.<strong>Chill Structure</strong>",
+				}
+      }
+    }
+  },
+  ES: {
+    value1: "Contenido",
+    value2: "Contenido JSON funciona."
+  }
+};
