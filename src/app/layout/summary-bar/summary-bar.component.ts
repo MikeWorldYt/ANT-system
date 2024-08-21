b64dd1618ac2369243515de20249222a8f0da683
mikeworldyt <diazmig98@gmail.com>
2024-08-21 15:39:04 -0600

    chore(summary-bar): conecting service for get article intersection name

--- src/app/layout/summary-bar/summary-bar.component.ts
+++ src/app/layout/summary-bar/summary-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { IntersectionService } from '../../services/IntersectionObserver.service';
 
 @Component({
   selector: 'app-summary-bar',
@@ -7,6 +8,21 @@ import { Component } from '@angular/core';
   templateUrl: './summary-bar.component.html',
   styleUrl: './summary-bar.component.css'
 })
-export class SummaryBarComponent {
+export class SummaryBarComponent implements AfterViewInit {
+  // ▲ SERVICES ▲
+  constructor(
+    private intersectionService: IntersectionService
+  ) { }
 
+  //  ▬▬▬ Intersection Section <section>
+  idActive: string = '';
+
+  // ████ AfterViewInit
+
+  ngAfterViewInit() {
+    // Intersection Observer function
+    this.intersectionService.getCurrentId().subscribe(id => {
+      this.idActive = id;
+    })
+  }
 }
