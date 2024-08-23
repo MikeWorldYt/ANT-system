35014f3f352764c7787dce13baca0ae770aeba26
mikeworldyt <diazmig98@gmail.com>
2024-08-22 18:56:38 -0600

    chore(language) : controling the currentLanguage

--- src/app/layout/summary-bar/summary-bar.component.ts
+++ src/app/layout/summary-bar/summary-bar.component.ts
@@ -1,6 +1,13 @@
-import { AfterViewInit, Component } from '@angular/core';
-import { IntersectionService } from '../../services/IntersectionObserver.service';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { NgClass } from '@angular/common';
+
+// ▲ SERVICES ▲
+import { LanguageService } from '../../services/lenguaje.service';
+import { IntersectionService } from '../../services/IntersectionObserver.service';
+import { Language } from '../../services/language.types';
+
+// ▲ CONTENT ▲
+import { content } from '../../docs/content/content';
 
 @Component({
   selector: 'app-summary-bar',
@@ -11,17 +18,21 @@ import { NgClass } from '@angular/common';
   templateUrl: './summary-bar.component.html',
   styleUrl: './summary-bar.component.css'
 })
-export class SummaryBarComponent implements AfterViewInit {
+export class SummaryBarComponent implements OnInit, AfterViewInit {
   // ▲ SERVICES ▲
   constructor(
-    private intersectionService: IntersectionService
+    private intersectionService: IntersectionService,
+    private languageService: LanguageService
   ) { }
-
 
   //  ▬▬▬ Intersection Section <section>
   idActive: string = '';
 
   // ████ AfterViewInit
+
+  ngOnInit(): void {
+
+  }
 
   ngAfterViewInit() {
     // Intersection Observer function
@@ -29,4 +40,15 @@ export class SummaryBarComponent implements AfterViewInit {
       this.idActive = id;
     })
   }
+
+  // ▬▬▬ Language
+  currentLanguage: Language = 'ES';
+
+  // ███ Language Controller ███
+
+  private isValidLanguage(language: string): language is Language {
+    return language === 'EN' || language === 'ES';
+  }
+
+
 }
