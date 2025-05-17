7f397476a5936651e26feb36f6f3dfc406134718
mikeworldyt <diazmig98@gmail.com>
2025-05-16 18:43:48 -0600

    chore(apps): Create subtree

--- Documentation/Angular/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
