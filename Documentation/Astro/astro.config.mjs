5b2bb102a058d4dd81642d5ff67119d760e2105c
mikeworldyt <diazmig98@gmail.com>
2025-06-30 20:36:52 -0600

    feat(Documentation/Astro): add React integration and update package dependencies

--- Documentation/Astro/astro.config.mjs
+++ Documentation/Astro/astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
+		react(),
 		starlight({
 			title: 'My Docs',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/MikeWorldYt/ANT-system/' }],
