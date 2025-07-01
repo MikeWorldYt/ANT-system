911dd175a6f563595a6c230a2f4131a8f600b249
mikeworldyt <diazmig98@gmail.com>
2025-06-30 18:35:33 -0600

    feat(Documentation/Astro): Initialize Astro documentation site with essential configurations and assets

---
+++ Documentation/Astro/astro.config.mjs
@@ -0,0 +1,31 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'My Docs',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			// sidebar: [
+			// 	{
+			// 		label: 'Guides',
+			// 		items: [
+			// 			// Each item here is one entry in the navigation menu.
+			// 			{ label: 'Example Guide', slug: 'guides/example' },
+			// 			{ label: 'Second Guide', slug: 'guides/faq' },
+			// 		],
+			// 	},
+			// 	{
+			// 		label: 'Reference',
+			// 		autogenerate: { directory: 'reference' },
+			// 	},
+			// ],
+			customCss: [
+				'./src/styles/custom.css',
+			],
+		}),
+	],
+});
